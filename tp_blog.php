834fe90418f7988b143f115990ee0876b9f42c81
BOB\Anthony <Anthony.briot.11@gmail.com>
2016-11-17 11:52:39 +0100

    Modifications apportés après le cours du 17/11/2016

--- tp_blog.php
+++ tp_blog.php
@@ -12,8 +12,57 @@
 			<h1>Blog</h1>
 		</header>
 		
+		<div class="php">
+			<?php
+			date_default_timezone_set('Europe/Paris');
+					$jour = date('d');
+					$mois = date('m');
+					$annee = date('y');
+					$heure = date('h');
+					$minute = date('i');
+					
+					
+				//On vérifie que le formulaire est rempli	
+				if (isset($_POST['titre']) and isset($_POST['pseudo']) and isset($_POST['message']) and !empty($_POST['titre']) and !empty($_POST['pseudo']) 
+					and !empty($_POST['message'])){
+						//On ouvre le fichier avec a+ pour écrire dedans et le créer s'il n'existe pas
+						$f = (fopen("fichier.txt", 'a+'));
+						//On écrit sur une ligne du fichier
+						fputs($f, '<h2>'.$_POST['titre'].'</h2><br> <h3>'.$_POST['pseudo'].'</h3><br> Le ' .$jour.'/'.$mois.'/'.$annee.' à '.$heure.'h'.$minute.'. <br>' .$_POST['message'].'<br><br>');
+						//On ferme le fichier
+						fclose($f);
+					}
+				//On ré-ouvre le fichier, cette fois en mode lecture	
+				$f = (fopen("fichier.txt", 'r+'));
+				//On choisis de lire une ligne du fichier texte
+				$message = fgets($f);
+				//On affiche le message
+				echo $message;
+				//On ferme à nouveau le fichier texte
+				fclose($f);
+				
+				/* // test d'affichage de chaque valeur
+				
+				date_default_timezone_set('Europe/Paris');
+					$jour = date('d');
+					$mois = date('m');
+					$annee = date('y');
+					$heure = date('h');
+					$minute = date('i');
+				
+				echo '<h2>'.($_POST['titre'].'</h2>');
+				echo '<br/>';
+				echo '<h3>'.($_POST['pseudo'].'</h3>');
+				echo '<br/>';
+				echo 'Le ' .$jour.'/'.$mois.'/'.$annee.' à '.$heure.'h'.$minute.'.';
+				echo '<br/>';
+				echo ($_POST['message']);
+				*/
+			?>
+		</div>
+
 		<form action="#" method="POST">
-			<p>
+				<p>
 				<label for="nom">Pseudo : </label><input type="text" name="pseudo" />
 				<br/>
 				<br/>
@@ -28,26 +77,6 @@
 			</p>
 		</form>
 	</body>
-	<div class="php">
-		<?php
-
-			date_default_timezone_set('Europe/Paris');
-				$jour = date('d');
-				$mois = date('m');
-				$annee = date('y');
-				$heure = date('h');
-				$minute = date('i');
-			
-			echo '<h2>'.($_POST['titre'].'</h2>');
-			echo '<br/>';
-			echo '<h3>'.($_POST['pseudo'].'</h3>');
-			echo '<br/>';
-			echo 'Le ' .$jour.'/'.$mois.'/'.$annee.' à '.$heure.'h'.$minute.'.';
-			echo '<br/>';
-			echo ($_POST['message']);
-		?>
-</div>
-
 </html>
 
 
